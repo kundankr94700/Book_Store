0603a1f1e2b222eaa6d541422f176104fd6fd5e1
Kundan Kumar <kundankr94700@gmail.com>
2021-08-10 09:56:18 +0530

    first component

---
+++ src/FirstComponent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function FirstComponent() {
+    return (
+        <React.Fragment>
+            <div>
+        <h1>First App in React</h1>
+    </div>
+    </React.Fragment>
+        
+    )
+}
+
+export default FirstComponent
