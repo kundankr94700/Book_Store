da6887ccb6944988f80daa20343feec6093bf6c8
Kundan Kumar <kundankr94700@gmail.com>
2021-08-10 13:45:04 +0530

    bookStore Updates

--- src/Bookstore.js
+++ src/Bookstore.js
@@ -1,17 +1,30 @@
 import React from 'react'
+import './index.css'
 
-function Bookstore() {
+function Bookstore(props) {
     return (
-        <section>
-            <Book/>
+        <section >            
+            <Book photo={props.img} titles={props.title} authers={props.auther} prices={props.price}> </Book>
         </section>
     )
 }
-const Book=()=>{
-    return <article><Img></Img></article>
+const Book=(props)=>{
+    return (
+    <article>
+        <Img pic={props.photo}/>
+        <Title mytitle={props.titles}/>
+        <Auther myauther={props.authers}/>   
+        <Price myprice={props.prices}/> 
+        </article>
+    
+    )
 }
-const Img=()=>
-{
-    return <img src='https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL320_SR228,320_.jpg' width='100' height='100'/>
-}
+const Img=(props)=><img src={props.pic}/>
+
+const Auther=(props)=><h3> {props.myauther}</h3>
+
+const Title=(props)=> <h1> {props.mytitle}</h1>
+
+const Price=(props)=> <h3> {props.price}</h3>
+
 export default Bookstore
--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Bookstore from './Bookstore.js';
 import FirstComponent from './FirstComponent.js'
-
+import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <MyComponent/>   this is mycomponent that contains name age and */}
     {/* <FirstComponent/> this is how we use a component */}
-    <Bookstore/>
+    
+    <table rules='all'>
+      <tr  width='200'>
+        <td ><Bookstore img="https://images-eu.ssl-images-amazon.com/images/I/81l3rZK4lnL._AC_UL320_SR228,320_.jpg"title="Ikigai: The Japanese secret to a long and…" auther='Héctor García' price='290'/></td>
+        <td><Bookstore img="https://images-eu.ssl-images-amazon.com/images/I/71xBLRBYOiL._AC_UL200_SR200,200_.jpg" title="My First Library: Boxset of 10 Board Books…" auther='Board book' price='399'/></td>
+        <td><Bookstore img="https://images-eu.ssl-images-amazon.com/images/I/81cpDaCJJCL._AC_UL200_SR200,200_.jpg" title="The Psychology of Money" auther='Morgan Housel' price='254'/></td>
+     
+      </tr>
+      <tr>
+      <td><Bookstore img="https://images-eu.ssl-images-amazon.com/images/I/91VokXkn8hL._AC_UL200_SR200,200_.jpg" title="Rich Dad Poor Dad: What the Rich Teach…" auther='Mass Market Paperback' price='317'/></td>
+      <td><Bookstore img="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL200_SR200,200_.jpg" title="Atomic Habits: The life-changing million…" auther='James Clear' price='542'/></td>
+      <td><Bookstore img="https://images-eu.ssl-images-amazon.com/images/I/81j0Jii6mZL._AC_UL200_SR200,200_.jpg" title="Picture Books Collection for Early Learning…" auther='E C Axus' price='230'/></td>
+    
+      </tr>
+    </table>
   </React.StrictMode>,
   document.getElementById('root')
 );
